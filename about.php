54f6affb62ed7cafd4b2955f8c37770fa7217b4f
wuwa31 <wangwu@umich.edu>
2015-04-03 19:44:19 -0400

    Create about.php
    Stylesheet must be added

---
+++ about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="default.css">
+</head>
+<header>
+	<title>History</title>
+</header>
+<body>
+<?php
+	$Name = basename(__FILE__);
+	include ('header.php');
+?>
+	<h1>About Ann Arbor</h1>
+		<p>Ann Arbor was founded in 1824, and named after the wives of the village's founders and the strands of Bur Oak trees.
+		The University of Michigan's main campus moved to Ann Arbor from Detroit in 1837, and became a primary contributor to the city's growth.
+		During the 1960s-1970s, Ann Arbor became known for leftist political activism and served as a hub for the civil-rights movement and anti-Vietnam War movement. Today, Ann Arbor is home to over 43,000 Wolverines, who recieve a world-renowned education, and enjoy a social scene with the best bars, restaurants, and sporting events.
+		</p>
+</body>
+</html>
+		
+<?php
+	include ('footer.php');
+?>
+</body>
+</html>
