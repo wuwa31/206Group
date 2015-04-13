dee18607b7e2f405153b18d8f096301db9cdf146
marisax <marisax@umich.edu>
2015-04-12 22:51:05 -0400

    add map

--- restaurants.php
+++ restaurants.php
@@ -1,40 +1,161 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>Bar Map</title>
-  <link href="default.css" rel="stylesheet">
-  <link href="style.css" rel="stylesheet">
-  <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-  <script>
-    google.load('visualization', '1', { 'packages': ['map'] });
-    google.setOnLoadCallback(drawMap);
+	<head>
+		<meta charset="UTF-8">
+		<title>Restaurants</title>
+		<link href="default.css" rel="stylesheet">
+		<!-- <link href="sharedcss.css" rel="stylesheet"> -->
+		<script type="text/javascript" src="https://www.google.com/jsapi"></script>
+		<script>
+		    google.load('visualization', '1', { 'packages': ['map'] });
+		    google.setOnLoadCallback(drawMap);
 
-    function drawMap() {
-      var data = google.visualization.arrayToDataTable([
-        ['Lat', 'Long', 'Name'],
-        [42.274571, -83.732823, 'World of Beer'],
-        [42.274842, -83.733316, 'Blue Leprechaun'],
-        [42.274383, -83.734444, 'Ricks American Cafe'],
-        [42.274853, -83.734907, 'Good Time Charleys'],
-        [42.278878, -83.742416, 'Scorekeepers'],
-        [42.281313, -83.751344, 'Live'],
-        [42.279086, -83.748889, 'Rush Street'],
-        [42.280158, -83.751342, 'Cavern/Millennium Club'],
-        [42.279795, -83.749740, 'Old Town Tavern'],
-        [42.280534, -83.746747, 'Mash'],
-      ]);
+		    function drawMap() {
+		      var data = google.visualization.arrayToDataTable([
+		        ['Lat', 'Long', 'Name'],
+		        [42.280498, -83.746462, 'Aventura'],
+		        [42.278699, -83.748851, 'Gratzi'],
+		        [42.280665, -83.749586, 'Grizzly Peak'],
+		        [42.280635, -83.749280, 'Cafe Zola'],
+		        [42.279488, -83.750074, 'Fleetwood Diner'],
+		        [42.274343, -83.734859, 'Pizza House'],
+		        [42.277928, -83.741528, 'Hunter House'],
+		        [42.279790, -83.740653, 'Amas Bistro'],
+		        [42.282998, -83.735402, 'Angelos'],
+		        [42.279474, -83.744550, 'Isalita'],
+		        [42.279134, -83.748246, 'Jolly Pumpkin'],
+		        [42.279152, -83.745878, 'Jeruselam Garden'],
+		      ]);
 
-    var options = {showTip: true, useMapTypeControl: true, mapType: 'normal'};
+		    var options = {showTip: true, useMapTypeControl: true, mapType: 'normal'};
 
-    var map = new google.visualization.Map(document.getElementById('chart_div'));
+		    var map = new google.visualization.Map(document.getElementById('chart_div'));
 
-    map.draw(data, options);
-  };
-  </script>
-</head>
+		    map.draw(data, options);
+		  };
+		</script>
+	</head>
+	<body>
+		<? php
+			$Name = basename(__FILE__);
+			include("header.php");
+		?>
 
-  <body>
-    <div id="chart_div"></div>
-  </body>
+		<h1> Restaurants</h1> 
+
+		<div class="post">
+			<div class="post_title"><h3> Best Late night food </h3></div>
+				<div class="item">
+					<figure>
+						<img src="fleetwood.jpg">
+						<figcaption>300 S Ashley St, Ann Arbor, MI 48104</figcaption>
+					</figure>
+					<h4>Fleedwood Diner</h4>
+					<p>A cool 24 hour retro diner tucked away from main st, their hippie hash, hash browns topped with veggies and melted feta, is a must try! </p>
+				</div>	
+				<div class="item">
+					<figure>
+						<img src="pizzahouse.jpg">
+						<figcaption>618 Church St, Ann Arbor, MI 48104</figcaption>
+					</figure>
+					<h4>Pizza House</h4>
+					<p>Open until 4am! Pizza house is a classic Ann Arbor joint, get the feta bread as a late night snack!</p>
+				</div>		
+				<div class="item">
+					<figure>
+						<img src="hunterhouse.jpg">
+						<figcaption>609 E William St, Ann Arbor, MI 48104</figcaption>
+					</figure>
+					<h4>Hunter House</h4>
+					<p>A newcomer to the Ann Arbor scene, this burger joint is open Thursday-Saturday until 3am. Their classic sliders are the best around and they even deliver! </p>
+				</div>	
+				<div class="item">
+					<figure>
+						<img src="ama.jpg">
+						<figcaption>215 S State St, Ann Arbor, MI 48104</figcaption>
+					</figure>
+					<h4>Ama's Bistro</h4>
+					<p>Also a newcomer to Ann Arbor, this bistro is open late night on the weekend. Ama's has classic diner food with some amazing specials.</p>
+				</div>	
+		</div>
+		<div class="post">
+			<div class="post_title"><h3> Where do you go for fine dining? </h3></div>
+				<div class="item">
+					<figure>
+						<img src="aventura.jpg">
+						<figcaption>216 E Washington St,  Ann Arbor, MI 48104</figcaption>
+					</figure>
+					<h4>Aventura </h4>
+					<p>Aventura has some of the best food in Ann Arbor. The atmosphere isn't super fancy but you are sure to have a classy night. The food is a variety of small tapas plates. The cocktails are also great! The champagne sangria is a must try!</p>
+				</div>	
+				<div class="item">
+					<figure>
+						<img src="gratzi.jpg">
+						<figcaption>326 S Main St,  Ann Arbor, MI 48104</figcaption>
+					</figure>
+					<h4>Gratzi</h4>
+					<p>An elegant Italian place with a modern twist. This restaurant has amazing italian good, the tiramisu is amazing!</p>
+				</div>		
+				<div class="item">
+					<figure>
+						<img src="grizzly.jpg">
+						<figcaption>120 W Washington St, Ann Arbor, MI 48104</figcaption>
+					</figure>
+					<h4>Grizzly Peak</h4>
+					<p>An interesting take on a steak house, this restuarant is great for a classy night out! </p>
+				</div>	
+				<div class="item">
+					<figure>
+						<img src="zola.jpg">
+						<figcaption>112 W Washington St, Ann Arbor, MI 48104 </figcaption>
+					</figure>
+					<h4>Cafe Zola</h4>
+					<p>The best for a fancy brunch in Ann Arbor! The food and cocktails are amazing!</p>
+				</div>	
+		</div>
+
+		<div class="post">
+			<div class="post_title"><h3>Food you have to try before you graduate</h3></div>
+				<div class="item">
+					<figure> 
+						<img src="angelos.jpg">
+						<figcaption>1100 Catherine St, Ann Arbor, MI 48104</figcaption>
+					</figure> 	
+					<h4>Angelo's Brunch</h4>
+					<p> If you haven't crawled out of bed on a Sunday morning to eat at Angelo's, then do you even go to this school? Try their classic breakfast items or their specials. The Huevos Rancheros are a personal favorite. Be prepared to wait for a table but don't worry its worth it.</p>
+				</div>
+				<div class="item">
+					<figure> 
+						<img src="isalita.jpg">
+						<figcaption>314 E Liberty St, Ann Arbor, MI 48104</figcaption>
+					</figure> 
+					<h4>Guac and Margarita's at Isalita</h4>
+					<p>	Their frozen blood orange margarita's are like adult slurpies! Come to happy hour to get them half off.	</p>
+				</div>
+				<div class="item">
+					<figure> 
+						<img src="jolly.jpg">
+						<figcaption>311 S Main St, Ann Arbor, MI 48104</figcaption>
+					</figure> 
+					<h4>Truffle Pizza at Jolly Pumpkin</h4>
+					<p>Such a cool Ann Arbor establishment! Jolly Pumpkin combines great quality food with a pub atmosphere. Try the beer sampler and the pizza's are all amazing.</p>
+				</div>
+				<div class="item">
+					<figure> 
+						<img src="gardens.jpg">
+						<figcaption>307 S 5th Ave, Ann Arbor, MI 48104</figcaption>
+					</figure> 
+					<h4>Falafel at Jeruselam Gardens</h4>
+					<p>Jeruselam's Gardens is a mix of fresh food with classic middle eastern fares! Get their takeout on any night and the falafel is the best in Ann Arbor! </p>
+				</div>
+		</div>
+		
+	    <div id="chart_div"></div>
+
+		<? php
+			include("footer.php");
+		?>
+
+	</body> 
 </html>
+
