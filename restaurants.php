1b3b4396c3df928f02c0aed6804ceffbb9f3bf36
marisax <marisax@umich.edu>
2015-04-12 23:26:24 -0400

    change for styling

--- restaurants.php
+++ restaurants.php
@@ -4,7 +4,8 @@
 		<meta charset="UTF-8">
 		<title>Restaurants</title>
 		<link href="default.css" rel="stylesheet">
-		<!-- <link href="sharedcss.css" rel="stylesheet"> -->
+		<link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
+		<link href="sharedstyle.css" rel="stylesheet">
 		<script type="text/javascript" src="https://www.google.com/jsapi"></script>
 		<script>
 		    google.load('visualization', '1', { 'packages': ['map'] });
@@ -41,113 +42,168 @@
 			include("header.php");
 		?>
 
-		<h1> Restaurants</h1> 
+		<h1>Ann Arbor Restaurants</h1> 
+		
+		<h3> Best Late night food </h3>
 
 		<div class="post">
-			<div class="post_title"><h3> Best Late night food </h3></div>
-				<div class="item">
-					<figure>
-						<img src="fleetwood.jpg">
-						<figcaption>300 S Ashley St, Ann Arbor, MI 48104</figcaption>
-					</figure>
-					<h4>Fleedwood Diner</h4>
-					<p>A cool 24 hour retro diner tucked away from main st, their hippie hash, hash browns topped with veggies and melted feta, is a must try! </p>
-				</div>	
-				<div class="item">
-					<figure>
-						<img src="pizzahouse.jpg">
-						<figcaption>618 Church St, Ann Arbor, MI 48104</figcaption>
-					</figure>
-					<h4>Pizza House</h4>
-					<p>Open until 4am! Pizza house is a classic Ann Arbor joint, get the feta bread as a late night snack!</p>
-				</div>		
-				<div class="item">
-					<figure>
-						<img src="hunterhouse.jpg">
-						<figcaption>609 E William St, Ann Arbor, MI 48104</figcaption>
-					</figure>
-					<h4>Hunter House</h4>
-					<p>A newcomer to the Ann Arbor scene, this burger joint is open Thursday-Saturday until 3am. Their classic sliders are the best around and they even deliver! </p>
-				</div>	
-				<div class="item">
-					<figure>
-						<img src="ama.jpg">
-						<figcaption>215 S State St, Ann Arbor, MI 48104</figcaption>
-					</figure>
-					<h4>Ama's Bistro</h4>
-					<p>Also a newcomer to Ann Arbor, this bistro is open late night on the weekend. Ama's has classic diner food with some amazing specials.</p>
-				</div>	
-		</div>
+			<header><h4>Fleedwood Diner</h4></header>
+			<section class="address">
+				<p>300 S Ashley St, Ann Arbor, MI 48104</p>
+			</section>
+			<section class="content">
+				<p>A cool 24 hour retro diner tucked away from main st, their hippie hash, hash browns topped with veggies and melted feta, is a must try! </p>
+			</section> 
+			<section class="sidebar">
+				<img src="fleetwood.jpg">
+			</section>
+		</div>	
+		
 		<div class="post">
-			<div class="post_title"><h3> Where do you go for fine dining? </h3></div>
-				<div class="item">
-					<figure>
-						<img src="aventura.jpg">
-						<figcaption>216 E Washington St,  Ann Arbor, MI 48104</figcaption>
-					</figure>
-					<h4>Aventura </h4>
-					<p>Aventura has some of the best food in Ann Arbor. The atmosphere isn't super fancy but you are sure to have a classy night. The food is a variety of small tapas plates. The cocktails are also great! The champagne sangria is a must try!</p>
-				</div>	
-				<div class="item">
-					<figure>
-						<img src="gratzi.jpg">
-						<figcaption>326 S Main St,  Ann Arbor, MI 48104</figcaption>
-					</figure>
-					<h4>Gratzi</h4>
-					<p>An elegant Italian place with a modern twist. This restaurant has amazing italian good, the tiramisu is amazing!</p>
-				</div>		
-				<div class="item">
-					<figure>
-						<img src="grizzly.jpg">
-						<figcaption>120 W Washington St, Ann Arbor, MI 48104</figcaption>
-					</figure>
-					<h4>Grizzly Peak</h4>
-					<p>An interesting take on a steak house, this restuarant is great for a classy night out! </p>
-				</div>	
-				<div class="item">
-					<figure>
-						<img src="zola.jpg">
-						<figcaption>112 W Washington St, Ann Arbor, MI 48104 </figcaption>
-					</figure>
-					<h4>Cafe Zola</h4>
-					<p>The best for a fancy brunch in Ann Arbor! The food and cocktails are amazing!</p>
-				</div>	
-		</div>
+			<header><h4>Pizza House</h4></header>
+			<section class="address">
+				<p>618 Church St, Ann Arbor, MI 48104</p>
+			</section>
+			<section class="content">
+				<p>Open until 4am! Pizza house is a classic Ann Arbor joint, get the feta bread as a late night snack!</p>
+			</section> 
+			<section class="sidebar">
+				<img src="pizzahouse.jpg">
+			</section>
+		</div>		
+				
+		<div class="post">
+			<header><h4>Hunter House</h4></h4>
+			<section class="address">
+				<p>609 E William St, Ann Arbor, MI 48104</p>
+			</section>
+			<section class="content">
+				<p>A newcomer to the Ann Arbor scene, this burger joint is open Thursday-Saturday until 3am. Their classic sliders are the best around and they even deliver! </p>
+			</section> 
+			<section class="sidebar">
+				<img src="hunterhouse.jpg">
+			</section>
+		</div>	
+				
+		<div class="post">
+			<header><h4>Ama's Bistro</h4></header>
+			<section class="address">
+				<p>215 S State St, Ann Arbor, MI 48104</p>
+			</section>
+			<section class="content">
+				<p>Also a newcomer to Ann Arbor, this bistro is open late night on the weekend. Ama's has classic diner food with some amazing specials.</p>
+			</section> 
+			<section class="sidebar">
+				<img src="ama.jpg">
+			</section>
+		</div>	
+		
+		<h3> Where do you go for fine dining? </h3>
+		
+		<div class="post">
+			<header><h4>Aventura </h4></header>
+			<section class="address">
+				<p>216 E Washington St,  Ann Arbor, MI 48104</p>
+			</section>
+			<section class="content">
+				<p>Aventura has some of the best food in Ann Arbor. The atmosphere isn't super fancy but you are sure to have a classy night. The food is a variety of small tapas plates. The cocktails are also great! The champagne sangria is a must try!</p>
+			</section> 
+			<section class="sidebar">
+				<img src="aventura.jpg">
+			</section>
+		</div>	
+				
+		<div class="post">
+			<header><h4>Gratzi</h4></header>
+			<section class="address">
+				<p>326 S Main St,  Ann Arbor, MI 48104</p>			
+			</section>
+			<section class="content">
+				<p>An elegant Italian restaurant with a modern twist. This restaurant has amazing italian good, the tiramisu is amazing!</p>
+			</section> 
+			<section class="sidebar">
+				<img src="gratzi.jpg">
+			</section>
+		</div>		
+		
+		<div class="post">
+			<header><h4>Grizzly Peak</h4></header>
+			<section class="address">
+				<p>120 W Washington St, Ann Arbor, MI 48104</p>
+			</section>
+			<section class="content">
+				<p>An interesting take on a steak house, this restuarant is great for a classy night out! </p>
+			</section> 
+			<section class="sidebar">
+				<img src="grizzly.jpg">
+			</section>
+		</div>	
+				
+		<div class="post">
+			<header><h4>Cafe Zola</h4></header>
+			<section class="address">
+				<p>112 W Washington St, Ann Arbor, MI 48104 </p>
+			</section>
+			<section class="content">
+				<p>The best for a fancy brunch in Ann Arbor! The food and cocktails are amazing!</p>
+			</section> 
+			<section class="sidebar">
+				<img src="zola.jpg">
+			</section>
+		</div>	
+
+		<h3>Food you have to try before you graduate</h3>
 
 		<div class="post">
-			<div class="post_title"><h3>Food you have to try before you graduate</h3></div>
-				<div class="item">
-					<figure> 
-						<img src="angelos.jpg">
-						<figcaption>1100 Catherine St, Ann Arbor, MI 48104</figcaption>
-					</figure> 	
-					<h4>Angelo's Brunch</h4>
-					<p> If you haven't crawled out of bed on a Sunday morning to eat at Angelo's, then do you even go to this school? Try their classic breakfast items or their specials. The Huevos Rancheros are a personal favorite. Be prepared to wait for a table but don't worry its worth it.</p>
-				</div>
-				<div class="item">
-					<figure> 
-						<img src="isalita.jpg">
-						<figcaption>314 E Liberty St, Ann Arbor, MI 48104</figcaption>
-					</figure> 
-					<h4>Guac and Margarita's at Isalita</h4>
-					<p>	Their frozen blood orange margarita's are like adult slurpies! Come to happy hour to get them half off.	</p>
-				</div>
-				<div class="item">
-					<figure> 
-						<img src="jolly.jpg">
-						<figcaption>311 S Main St, Ann Arbor, MI 48104</figcaption>
-					</figure> 
-					<h4>Truffle Pizza at Jolly Pumpkin</h4>
-					<p>Such a cool Ann Arbor establishment! Jolly Pumpkin combines great quality food with a pub atmosphere. Try the beer sampler and the pizza's are all amazing.</p>
-				</div>
-				<div class="item">
-					<figure> 
-						<img src="gardens.jpg">
-						<figcaption>307 S 5th Ave, Ann Arbor, MI 48104</figcaption>
-					</figure> 
-					<h4>Falafel at Jeruselam Gardens</h4>
-					<p>Jeruselam's Gardens is a mix of fresh food with classic middle eastern fares! Get their takeout on any night and the falafel is the best in Ann Arbor! </p>
-				</div>
+			<header><h4>Angelo's Brunch</h4></header>
+			<section class="address">
+				<p>1100 Catherine St, Ann Arbor, MI 48104</p>
+			</section>
+			<section class="content">
+				<p> If you haven't crawled out of bed on a Sunday morning to eat at Angelo's, then do you even go to this school? Try their classic breakfast items or their specials. The Huevos Rancheros are a personal favorite. Be prepared to wait for a table but don't worry its worth it.</p>
+			</section> 
+			<section class="sidebar">
+				<img src="angelos.jpg">
+			</section>
+		</div>
+		
+		<div class="post">
+			<header><h4>Guac and Margarita's at Isalita</h4></header>
+			<section class="address">
+				<p>314 E Liberty St, Ann Arbor, MI 48104</p>
+			</section>
+			<section class="content">
+				<p>	Their frozen blood orange margarita's are like adult slurpies! Come to happy hour to get them half off.	</p>
+			</section> 
+			<section class="sidebar">
+				<img src="isalita.jpg">
+			</section>
+		</div>
+		
+		<div class="post">
+			<header><h4>Truffle Pizza at Jolly Pumpkin</h4></header>
+			<section class="address">
+				<p>311 S Main St, Ann Arbor, MI 48104</p>
+			</section>
+			<section class="content">
+				<p>Such a cool Ann Arbor establishment! Jolly Pumpkin combines great quality food with a pub atmosphere. Try the beer sampler and the pizza's are all amazing.</p>
+			</section> 
+			<section class="sidebar">
+				<img src="jolly.jpg">
+			</section>
+		</div>
+		
+		<div class="post">
+			<header><h4>Falafel at Jeruselam Gardens</h4></header>
+			<section class="address">
+				<p>307 S 5th Ave, Ann Arbor, MI 48104</p>
+			</section>
+			<section class="content">
+				<p>Jeruselam's Gardens is a mix of fresh food with classic middle eastern fares! Get their takeout on any night and the falafel is the best in Ann Arbor! </p>
+			</section> 
+			<section class="sidebar">
+				<img src="gardens.jpg">
+			</section>
 		</div>
 		
 	    <div id="chart_div"></div>
