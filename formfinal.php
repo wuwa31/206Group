9602c1a9ab22c7d3565ec707639f54e3df16c83a
marisax <marisax@umich.edu>
2015-04-14 08:57:16 -0400

    chuckry update and rename

--- formfinal.php
+++ formfinal.php
@@ -1,26 +1,39 @@
 <!DOCTYPE html>
+<?php $currentPage = basename(__FILE__); ?>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Form</title>
 	<link href="default.css" rel="stylesheet">
-	<link href="style.css" rel="stylesheet">
 	<link href="formPage.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="navStyle.css">
+	<link href='http://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
 	<script type="text/javascript">
-		 function checkEmail(){
+		function checkEmail(){
+			var em=document.getElementById('em');
+			var em2=document.getElementById('em2');
 		 	if (em.value != em2.value){
 				alert("The emails must match!");
 				return false;
 			}
+		function checkAge(){
+  			var age=document.getElementById('age');
+  			if (isNaN(age)){
+    			alert("Must input numbers");
+    			return false;
+  			}
 		}
+	}
 	</script>
 </head>
 <body>
-<form action="gotIt.php" method = "POST" onsubmit = "return checkEmail()">
+<form action="gotIt.php" method = "POST" onsubmit = "return checkEmail()", "return checkAge()">
 <h1>Ann Arbor Social Scene Survey</h1>	
+<?php include("header.php"); ?>
 <fieldset>
 	<legend></legend>
-	<label>Name: <input type = "name" name = "name" value = "Brian" required/></label>
+	<label>Name: <input type = "name" name = "name" id="name" value = "Brian" required/></label>
+	<label>Age: <input type = "numnber" name = "age" id="age" required/></label>
 	<label>Email:<input type = "email" name = "email" id = "em" required/></label>
 	<label>Confirm Email:<input type = "email" name = "confirm" id = "em2" required/></label>
 	
@@ -47,5 +60,6 @@
 </fieldset>
 
 </form>	
+<?php include("footer.php"); ?>
 </body>
 </html>
